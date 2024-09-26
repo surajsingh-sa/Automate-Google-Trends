88feff632cc88be9b0c49514c626819c9fa7aad4
Suraj Singh <suraj.singh@solutionanalysts.com>
2024-09-26 11:56:16 +0530

    filter changes

--- Automate_Google_Trends.py
+++ Automate_Google_Trends.py
@@ -17,9 +17,9 @@ def setup_driver():
     driver.maximize_window()
     return driver
 
-def open_google_trends(driver):
+def open_google_trends(driver, search_input):
     # Open the Google Trends website
-    driver.get('https://trends.google.com/trends/?geo=DE&q=blockchain&hl=en')
+    driver.get(f'https://trends.google.com/trends/?date=today 3-m&q={search_input}&hl=en')
 
 def search_term(driver, term):
     # Wait for the search input to be available and locate it by its id
@@ -67,24 +67,6 @@ def scroll_to_related_queries_section(driver):
             driver.execute_script("window.scrollBy(0, 500);")
             time.sleep(1)  # Pause for the scroll to take effect
 
-def select_germany_filter(driver):
-    # Locate the dropdown element
-    dropdown_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//div[contains(@class, '_md-select-value')]"))
-    )
-
-    # Click the dropdown to open options
-    dropdown_element.click()
-
-    # Wait for the options to become visible
-    time.sleep(2)
-
-    # Locate and select "Germany" from the options
-    germany_option = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//md-option//span[contains(text(), 'Germany')]"))
-    )
-    germany_option.click()
-
 def repeat_search_and_download(driver, term):
     # Locate the new search input for adding a new search term
     new_search_box = WebDriverWait(driver, 10).until(
@@ -103,9 +85,6 @@ def repeat_search_and_download(driver, term):
     # Wait for the new search results to load
     time.sleep(5)
 
-    # Call the function to select Germany
-    # select_germany_filter(driver)
-    time.sleep(1)
     # Scroll to the 'Related queries' section
     related_queries_section = scroll_to_related_queries_section(driver)
 
@@ -124,12 +103,13 @@ def repeat_search_and_download(driver, term):
 
 def main():
     # Main function to run the workflow
+    search_input = input("Please enter a search term: ")
     driver = setup_driver()
     try:
-        open_google_trends(driver)
-        search_term(driver, "blockchain")
+        open_google_trends(driver, search_input)
+        search_term(driver, search_input)
         download_csv(driver)
-        repeat_search_and_download(driver, "blockchain")
+        repeat_search_and_download(driver, search_input)
     finally:
         # Close the browser
         driver.quit()
